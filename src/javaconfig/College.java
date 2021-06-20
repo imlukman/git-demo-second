19e05be7875e28969cb4c706824d0f71078c36f0
iamlukman <lukmandalal@gmail.com>
2021-06-20 20:32:03 +0530

    minor changes

--- src/javaconfig/College.java
+++ src/javaconfig/College.java
@@ -2,6 +2,8 @@ package javaconfig;
 
 import org.springframework.stereotype.Component;
 
+//author = lukman
+
 @Component
 public class College {
 	private final String WELCOME_GREET="Welcome here where we train on hard work";
